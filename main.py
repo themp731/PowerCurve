c0432780acb90f5ba29c2a089254d8ede006ec68
MP <themp731@gmail.com>
2025-07-14 14:50:35 -0400

    Starting to get activity data

--- main.py
+++ main.py
@@ -16,22 +16,26 @@ STRAVA_CLIENT_ID = os.getenv('STRAVA_CLIENT_ID')
 STRAVA_CLIENT_SECRET = os.getenv('STRAVA_CLIENT_SECRET')
 STRAVA_REDIRECT_URI = os.getenv('STRAVA_REDIRECT_URI')
 
+# Starts the OATH2.0 flow with Strava
 @app.route("/")
 def home():
-    return '<a href="/authorize">Connect With Strava</a>'
+    print("Home Route Hit")
+    return '<h1>Hello from PowerCurve!</h1><p><a href="/authorize">Authorize with Strava</a></p>'
 
+# Authorizing the Application to work with your strava
 @app.route("/authorize")
 def authorize():
     auth_url = (
         f"https://www.strava.com/oauth/authorize"
-        f"?client_id={CLIENT_ID}"
+        f"?client_id={STRAVA_CLIENT_ID}"
         f"&response_type=code"
-        f"&redirect_uri={REDIRECT_URI}"
+        f"&redirect_uri={STRAVA_REDIRECT_URI}"
         f"&approval_prompt=force"
         f"&scope=read,activity:read"
     )
     return redirect(auth_url)
 
+# Sends you back home and lets you know what happend with your code request
 @app.route("/callback")
 def callback():
     code = request.args.get('code')
@@ -52,5 +56,8 @@ def callback():
     else:
         return "Authorization failed. No access token received.", 400
 
+# Grabbing data from specific activities to start
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5050)
